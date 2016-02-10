fff5008df4b3f37d96783c573def94dc06c8f1df
goncalvesjoao <goncalves.joao@gmail.com>
2016-02-10 13:56:09 +0000

    toxy configurable

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ const express = require('express');
 const setupToxy = require('./setupToxy');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3030;
+const toxyOn = false;
 
-// const toxyConfig = {
-//   port: process.env.PORT || 3000,
-// };
+const toxyConfig = {
+  port: process.env.PORT || 3000,
+};
 
 // ******************************* BOOTSTRAP ***********************************
 
 routes(app);
 
-// app.listen(port, setupToxy.bind(null, port, toxyConfig));
-
-app.listen(port, () => {
-  console.log(`Up and kicking ass on port ${ port }`);
-});
+if (toxyOn) {
+  app.listen(port, setupToxy.bind(null, port, toxyConfig));
+} else {
+  app.listen(toxyConfig.port, () => {
+    console.log(`Up and kicking ass on port ${ toxyConfig.port }`);
+  });
+}
