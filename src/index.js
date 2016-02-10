2db8b47be9190fe7be3adfb456c46652c20b5528
goncalvesjoao <goncalves.joao@gmail.com>
2016-02-10 13:52:14 +0000

    removing toxy

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ const express = require('express');
 const setupToxy = require('./setupToxy');
 
 const app = express();
-const port = 3030;
+const port = process.env.PORT || 3000;
 
-const toxyConfig = {
-  port: process.env.PORT || 3000,
-};
+// const toxyConfig = {
+//   port: process.env.PORT || 3000,
+// };
 
 // ******************************* BOOTSTRAP ***********************************
 
 routes(app);
 
-app.listen(port, setupToxy.bind(null, port, toxyConfig));
+// app.listen(port, setupToxy.bind(null, port, toxyConfig));
 
-// app.listen(port, () => {
-//   console.log(`Up and kicking ass on port ${ port }`);
-// });
+app.listen(port, () => {
+  console.log(`Up and kicking ass on port ${ port }`);
+});
